c155ed56c90ceea82fd995bfe8d7a6fd6e9ab830
Zachery Brady <bradyzp@dynamicgravitysystems.com>
2019-12-31 19:14:40 -0700

    Add simple postRequest method to UnleashedRequestHandler

--- src/main/java/net/jastrab/unleashed/api/http/UnleashedRequestHandler.java
+++ src/main/java/net/jastrab/unleashed/api/http/UnleashedRequestHandler.java
@@ -21,16 +21,34 @@ public class UnleashedRequestHandler {
     private final HttpClient client = HttpClient.newHttpClient();
     private final ObjectMapper mapper = UnleashedObjectMapper.getInstance().getMapper();
 
+    public <T> Optional<UnleashedResponse<T>> getResponse(HttpRequest request, Class<T> target) {
+        try {
+            HttpResponse<Supplier<UnleashedResponse<T>>> response = client.send(request, new JsonBodyHandler<>(target));
+            return Optional.of(response.body().get());
+        } catch (IOException | InterruptedException e) {
+            return Optional.empty();
+        }
+    }
+
+    public <T> Optional<T> postRequest(HttpRequest request, Class<T> target) {
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            T responseObject = mapper.readValue(response.body(), target);
+            return Optional.of(responseObject);
+        } catch (IOException | InterruptedException e) {
+            LOGGER.error("Exception occurred while executing POST request", e);
+            return Optional.empty();
+        }
+    }
 
     public class JsonBodyHandler<W> implements HttpResponse.BodyHandler<Supplier<UnleashedResponse<W>>> {
-
         private final Class<W> target;
 
         public JsonBodyHandler(Class<W> target) {
             this.target = target;
         }
 
-        private  HttpResponse.BodySubscriber<Supplier<UnleashedResponse<W>>> jsonHandler(Class<W> type, int statusCode) {
+        private HttpResponse.BodySubscriber<Supplier<UnleashedResponse<W>>> jsonHandler(Class<W> type, int statusCode) {
             HttpResponse.BodySubscriber<InputStream> upstream = HttpResponse.BodySubscribers.ofInputStream();
             JavaType parType = mapper.getTypeFactory().constructParametricType(UnleashedResponse.class, type);
 
@@ -51,15 +69,6 @@ public class UnleashedRequestHandler {
         @Override
         public HttpResponse.BodySubscriber<Supplier<UnleashedResponse<W>>> apply(HttpResponse.ResponseInfo responseInfo) {
             return jsonHandler(target, responseInfo.statusCode());
-        }
-    }
-
-    public <T> Optional<UnleashedResponse<T>> getResponse(HttpRequest request, Class<T> target) {
-        try {
-            HttpResponse<Supplier<UnleashedResponse<T>>> response = client.send(request, new JsonBodyHandler<>(target));
-            return Optional.of(response.body().get());
-        } catch (IOException | InterruptedException e) {
-            return Optional.empty();
         }
     }
 
